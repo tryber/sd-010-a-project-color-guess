fb429516fc3ce9e5b6715308db5f1025250539e0
Lucas André <lucas.cancado89@gmail.com>
2021-03-11 18:32:47 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function circleColors() {
+window.onload = function answer() {
   const circles = document.getElementsByClassName('ball');
-  for (let index = 0; index < circles.length; index += 1) {
-    circles[index].style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  const index = Math.ceil(Math.random() * 6) - 1;
+  circles[index].id = 'correct';
+  for (let i = 0; i < circles.length; i += 1) {
+    circles[i].style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+  }
+  const rgbColor = document.getElementById('correct').style.backgroundColor;
+  document.getElementById('rgb-color').innerText = rgbColor;
+};
+
+function attempt(event) {
+  if (event.target.id === 'correct') {
+    document.getElementById('answer').innerText = 'Acertou!';
+  } else {
+    document.getElementById('answer').innerText = 'Errou! Tente novamente!';
   }
 }
 
-window.onload = circleColors();
+document.getElementById('colors-container').addEventListener('click', attempt);
