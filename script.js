a92d15126faf4c0b53f39b260c7b9816eb8576cb
Gui-lira <gui.lira.martins@gmail.com>
2021-03-11 18:32:40 -0300

    3 e 4

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const colorText = document.createElement('h2');
 document.querySelector('.colorText').appendChild(colorText);
 colorText.id = 'rgb-color';
-colorText.innerText = '(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')'
+colorText.innerText = '(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')';
+for (let counter = 1; counter <= 6; counter += 1) {
+    const ball = document.createElement('div');
+    ball.style.backgroundColor = 'rgb(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')';
+    document.querySelector('.colorsContainer').appendChild(ball);
+    ball.className = 'ball';
+}
