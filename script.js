8fd767f09f3567889d42857a364f96a22f54ccd7
Gui-lira <gui.lira.martins@gmail.com>
2021-03-11 18:50:06 -0300

    5

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ document.querySelector('.colorText').appendChild(colorText);
 colorText.id = 'rgb-color';
 colorText.innerText = '(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')';
 for (let counter = 1; counter <= 6; counter += 1) {
-    const ball = document.createElement('div');
-    ball.style.backgroundColor = 'rgb(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')';
-    document.querySelector('.colorsContainer').appendChild(ball);
-    ball.className = 'ball';
-}
+  const ball = document.createElement('div');
+  ball.style.backgroundColor = 'rgb(' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ', ' + parseInt(Math.random()*255) + ')';
+  document.querySelector('.colorsContainer').appendChild(ball);
+  ball.className = 'ball';
+};
+const colors = document.querySelectorAll('.ball');
+colors[Math.ceil(Math.random() * 6)].classList.add('answer');
+document.querySelector('.answer').style.backgroundColor = 'rgb' + colorText.innerText;
+const paragraph = document.createElement('p');
+document.body.appendChild(paragraph);
+paragraph.innerText = 'Escolha uma cor';
+document.querySelector('.colorsContainer').addEventListener('click', function(event) {
+    if (event.target.classList.contains('answer')) {
+        paragraph.innerText = 'Acertou!'
+    }
+    else {
+        paragraph.innerText = 'Errou! Tente novamente!'
+    }
+});
+
