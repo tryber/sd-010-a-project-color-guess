91d131a539997e4396db8a96ecca4325c412dbb9
Lucas André <lucas.cancado89@gmail.com>
2021-03-12 13:27:58 -0300

    Requisito 7 agooora foi

--- script.js
+++ script.js
@@ -22,19 +22,21 @@ function attempt(event) {
 
 document.getElementById('colors-container').addEventListener('click', attempt);
 
-function refreshPage() {
-  window.location.reload();
-}
+function refreshP() {
+  const c = document.getElementsByClassName('ball');
+  const index = Math.ceil(Math.random() * 6) - 1;
+  c[index].id = 'correct';
+  for (let i = 0; i < c.length; i += 1) {
+    const r = Math.random() * 255;
+    const g = Math.random() * 255;
+    const b = Math.random() * 255;
+    c[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
+  const rgbColor = document.getElementById('correct').style.backgroundColor;
+  document.getElementById('rgb-color').innerText = rgbColor;
+};
 
-document.getElementById('reset-game').addEventListener('click', refreshPage);
-
-if (sessionStorage.getItem('score') === null) {
-  sessionStorage.setItem('score', '0');
-} else {
-  sessionStorage.setItem('score', parseInt(sessionStorage.getItem('score'), 10) + 0);
-}
-
-document.getElementById('score').innerText = sessionStorage.getItem('score');
+document.getElementById('reset-game').addEventListener('click', refreshP);
 
 function score(event) {
   const tagScore = document.getElementById('score');
@@ -42,7 +44,6 @@ function score(event) {
     tagScore.innerText = parseInt(tagScore.innerText, 10) + 3;
     document.getElementById('correct').id = 'checked';
   }
-  sessionStorage.setItem('score', tagScore.innerText);
 }
 
 document.getElementById('colors-container').addEventListener('click', score);
