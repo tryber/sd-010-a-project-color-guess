aca8c90cf1a735cfb584c3688608a704811f82ac
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-12 01:22:19 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,8 @@
+function numerosAleatorios() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  const numero = document.querySelector('#rgb-color');
+  numero.innerHTML = `rgb(${r}, ${g}, ${b})`;
+}
+numerosAleatorios();
