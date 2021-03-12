55281bff0a06fb586df593d4a1606dd039482120
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-03-12 12:42:55 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
+let numbersRgb;
 function numerosAleatorios() {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
+  const ball = document.querySelectorAll('.ball');
   const numero = document.querySelector('#rgb-color');
-  numero.innerHTML = `rgb(${r}, ${g}, ${b})`;
+  console.log(ball[Math.floor(Math.random() * ball.length)].style.backgroundColor);
+  numero.innerText = ball[Math.floor(Math.random() * ball.length)].style.backgroundColor;
 }
-numerosAleatorios();
 
-function balls() {
-  for (let index = 1; index <= 6; index += 1) {
+function balls(numBer) {
+  for (let index = 1; index <= numBer; index += 1) {
     const ball = document.createElement('div');
     ball.classList.add('ball');
     document.querySelector('#fatherBalls').appendChild(ball);
   }
 }
-balls();
+balls(6);
+
+function changeColorsBalls() {
+  const balss = document.getElementsByClassName('ball');
+  for (let index = 0; index < balss.length; index += 1) {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const valor = `rgb(${r}, ${g}, ${b})`;
+    balss[index].style.backgroundColor = valor;
+  }
+}
+changeColorsBalls();
+numerosAleatorios();
+function clickBalls(){
+  const answer = document.getElementById('answer');
+  const ball = document.getElementsByClassName('ball');
+  for(let index = 0; index < ball.length; index += 1){
+    ball[index].addEventListener('click', () => {
+      const numero = document.querySelector('#rgb-color')
+      if(ball[index].style.backgroundColor === numero.innerHTML){
+        answer.innerHTML = 'Acertou!'
+      }else {
+        answer.innerHTML = 'Errou! tente novamente!'
+      }
+    })
+  }
+}
+clickBalls(); 
