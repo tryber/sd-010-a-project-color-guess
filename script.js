cf5217bac6d1cb4e48089575ab0815b645ddfdf2
Lucas André <lucas.cancado89@gmail.com>
2021-03-12 13:40:30 -0300

    Requisito 7 agooora tem que ir

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-window.onload = function answer() {
+function answer() {
   const c = document.getElementsByClassName('ball');
-  const index = Math.ceil(Math.random() * 6) - 1;
-  c[index].id = 'correct';
   for (let i = 0; i < c.length; i += 1) {
     const r = Math.random() * 255;
     const g = Math.random() * 255;
     const b = Math.random() * 255;
     c[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    c[i].id = '';
   }
+  const index = Math.ceil(Math.random() * 6) - 1;
+  c[index].id = 'correct';
   const rgbColor = document.getElementById('correct').style.backgroundColor;
   document.getElementById('rgb-color').innerText = rgbColor;
-};
+}
+
+window.onload = answer;
 
 function attempt(event) {
   if (event.target.id === 'correct' || event.target.id === 'checked') {
@@ -22,21 +25,7 @@ function attempt(event) {
 
 document.getElementById('colors-container').addEventListener('click', attempt);
 
-function refreshP() {
-  const c = document.getElementsByClassName('ball');
-  const index = Math.ceil(Math.random() * 6) - 1;
-  c[index].id = 'correct';
-  for (let i = 0; i < c.length; i += 1) {
-    const r = Math.random() * 255;
-    const g = Math.random() * 255;
-    const b = Math.random() * 255;
-    c[i].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  }
-  const rgbColor = document.getElementById('correct').style.backgroundColor;
-  document.getElementById('rgb-color').innerText = rgbColor;
-};
-
-document.getElementById('reset-game').addEventListener('click', refreshP);
+document.getElementById('reset-game').addEventListener('click', answer);
 
 function score(event) {
   const tagScore = document.getElementById('score');
