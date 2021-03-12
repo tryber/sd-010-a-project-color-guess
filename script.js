fbbe689c7fef85dd09080506ad176a56cc468bce
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-11 22:18:29 -0300

    fixed some lint inssues

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 /* random color */
-let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-let balls = document.querySelectorAll('.ball');
+const balls = document.querySelectorAll('.ball');
 
 function generateRandomColor() {
-    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
 }
 
