bced85adb75fc4f92375547c32743a1d3569be19
Gabriel Ferreira <glucas709@gmail.com>
2021-03-14 22:14:52 -0300

    Requisito 05 - Conferindo se é a cor certa ou não

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const colorGuess = document.getElementById('rgb-color');
 const circles = document.querySelectorAll('.ball');
+const answer = document.getElementById('answer');
+const ballsGroup = document.getElementById('balls-container');
+
+window.onload = () => {
+  answer.innerHTML = 'Escolha uma cor';
+};
 
 function generateRgbColor() {
   const rgb1 = Math.floor(Math.random() * 255 + 1);
@@ -14,6 +20,21 @@ function setColorCircles() {
   for (let i = 0; i < circles.length; i += 1) {
     circles[i].style.backgroundColor = generateRgbColor();
   }
+  const correctBall = Math.floor(Math.random() * 6);
+  console.log(correctBall);
+  circles[correctBall].style.backgroundColor = colorGuess.innerHTML;
+}
+
+function checkColor() {
+  ballsGroup.addEventListener('click', (e) => {
+    const ball = e.target;
+    if (ball.style.backgroundColor === colorGuess.innerHTML) {
+      answer.innerHTML = 'Acertou!';
+    } else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
 }
 
 setColorCircles();
+checkColor();
