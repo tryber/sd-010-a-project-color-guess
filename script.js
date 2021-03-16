0fd061a8e5b39fe16ecd92476346a91f19e092d5
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-15 22:13:22 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// Gerando cor aleatória que deverá ser adivinhadas
+// Gerando cor aleatória
 const rgbColor = document.querySelector('#rgb-color');
 
 function generatesRandomRgb () {
-  rgbColor.innerText = `(${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)})`
+  const randomColor = `rgb(${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)}, ${Math.ceil(Math.random() * 254)})`
+
+  return randomColor;
 }
 
-generatesRandomRgb();
+// Gerar cores aleatórias dos círculos
+const balls = document.querySelectorAll('.ball')
+
+function generateBallsColor () {
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].style.backgroundColor = generatesRandomRgb();
+  }
+}
+
+generateBallsColor();
