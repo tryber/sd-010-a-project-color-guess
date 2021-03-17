0df9d1f6c449b9d51b2ff3e27e170b7b70336b8e
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-17 11:12:29 -0300

    add require 05

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function generateRandomColor() {
 for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = generateRandomColor();
 }
+
+// ---------- function display one random color in rgb text ----------
+const rgbText = document.querySelector('#rgb-color');
+
+function getRandomColorToText() {
+    const randomN = Math.floor(Math.random() * 6);
+    const randomC = balls[randomN].style.backgroundColor;
+    const randomCT = randomC.slice(3);
+    rgbText.innerText = randomCT;
+}
+
+getRandomColorToText();
+
+// ---------- function answer ----------
+const answer = document.querySelector('#answer');
+
+function checkAnswer(e) {
+    const target = e.target;
+    const text = document.querySelector('#rgb-color');
+    const textColor = `rgb${text.innerText}`
+    target.style.backgroundColor == textColor ? answer.innerText = "Acertou!" : answer.innerText = "Errou! Tente novamente!";
+}
+
+for (let index = 0; index < balls.length; index += 1) {
+    balls[index].addEventListener('click', checkAnswer);
+}
