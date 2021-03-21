aff83c8155a7927dcea7e3b9dd5ff22579571f06
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-21 19:14:40 -0300

    Colocando estilo na pagina e corrigindo erros dos lint

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const answer = document.getElementById('answer');
 const resetBtn = document.getElementById('reset-game');
 const score = document.getElementById('score');
 
-
 function randomRGB() {
   return Math.floor(Math.random() * 255);
+}
+
+function increaseScore() {
+  const scoreInt = parseInt(score.innerText, 10);
+  score.innerText = scoreInt + 3;
 }
 
 function initializeGame() {
@@ -14,13 +18,12 @@ function initializeGame() {
   const colorToBeGuessed = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
   rgbColor.innerText = colorToBeGuessed;
 
-  for (const ball of balls) {
-    ball.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
-    ball.addEventListener('click', (evt) => {
+  for (let ball = 0; ball < balls.length; ball += 1) {
+    balls[ball].style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+    balls[ball].addEventListener('click', (evt) => {
       if (evt.target.style.backgroundColor === colorToBeGuessed) {
         answer.innerText = 'Acertou!';
-        let scoreInt = parseInt(score.innerText);
-        score.innerText = scoreInt+3;
+        increaseScore();
       } else {
         answer.innerText = 'Errou! Tente novamente!';
       }
