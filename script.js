6c022ef07c8623bdaeab1f354d408e8e2af1264f
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-20 23:09:14 -0300

    Requisito 5 - clicar na bola exibe a resposta

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const rgbColor = document.getElementById('rgb-color');
 const balls = document.querySelectorAll('.ball');
+const answer = document.getElementById('answer');
 
 function randomRGB() {
   return Math.floor(Math.random()*255);
 }
 
-rgbColor.innerText = `(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+const colorToBeGuessed = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
+
+rgbColor.innerText = colorToBeGuessed;
 
 for (ball of balls) {
-  ball.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`
+  ball.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+  ball.addEventListener('click', (evt) => {
+    if (evt.target.style.backgroundColor === colorToBeGuessed) {
+      answer.innerText = 'Acertou!'
+    } else {
+      answer.innerText = 'Errou! Tente novamente!'
+    }
+  })
 }
+
+//replace one random ball with colorToBeGuessed
+let randomPosition = Math.floor(Math.random()*6);
+balls[randomPosition].style.backgroundColor = colorToBeGuessed;
