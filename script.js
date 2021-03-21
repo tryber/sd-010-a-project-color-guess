6aa20a4f72eff10c0691b2ec432af6170ecec2a8
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-20 21:23:20 -0300

    Setimo requisito

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const rgbColor = document.querySelector('#rgb-color');
 const colorOptions = document.querySelector('#color-options');
-const randomAnswer = Math.floor(Math.random() * 5);
 const answer = document.querySelector('#answer');
 const resetBtn = document.querySelector('#reset-game');
+let counter = 0;
+const showCounter = document.createElement('h2');
+const counterSection = document.querySelector('#counter-section');
 
 const color = () => Math.ceil(Math.random() * 255);
 
@@ -11,12 +13,18 @@ function showColor() {
   // console.log(rgbColor.innerText);
 }
 
+function generateRandomAnswer() {
+  const randomAnswer = Math.floor(Math.random() * 5);
+  return randomAnswer;
+}
+
 const generateColor = (balls) => {
+  const actualAnswer = generateRandomAnswer();
   for (let index = 0; index < balls; index += 1) {
     const newElement = document.createElement('div');
     newElement.className = 'ball';
     newElement.id = `ball${index + 1}`;
-    if (index === randomAnswer) {
+    if (index === actualAnswer) {
       newElement.style.backgroundColor = `rgb${rgbColor.innerText}`;
     } else {
       newElement.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
@@ -28,9 +36,19 @@ const generateColor = (balls) => {
 function checkColor(checkAnswer) {
   if (checkAnswer.style.backgroundColor === `rgb${rgbColor.innerText}`) {
     answer.innerHTML = 'Acertou!';
+    counter += 3;
   } else {
     answer.innerHTML = 'Errou! Tente novamente';
   }
+  showCounter.innerText = counter;
+  // console.log(showCounter.innerText);
+  counterSection.appendChild(showCounter);
+}
+
+// console.log(counter);
+
+function deletePrevious() {
+  colorOptions.innerHTML = '';
 }
 
 colorOptions.addEventListener('click', (e) => {
@@ -51,7 +69,9 @@ colorOptions.addEventListener('click', (e) => {
 
 resetBtn.addEventListener('click', () => {
   showColor();
+  deletePrevious();
   generateColor(6);
+  answer.innerHTML = 'Escolha uma cor';
 });
 
 window.onload = () => {
