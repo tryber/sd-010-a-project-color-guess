e84443681b7dc0ee131614f3254eaae06205e864
johnpligeiro <johnpligeiro@gmail.com>
2021-04-28 18:28:43 -0300

    Requisito 5 - parte 1

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const balls = document.querySelectorAll('.ball');
+// const ballsContainer = document.querySelectorAll('#balls');
+const rgbTextDisplay = document.getElementById('rgb-color');
+
 const randomColors = () => (`rgb(${Math.random() * 254},
 ${Math.random() * 254},
 ${Math.random() * 254})`);
@@ -9,4 +12,24 @@ const randomize = () => {
     itemPaleta.style.backgroundColor = `${randomColors()}`;
   });
 };
-randomize();
+
+const load = () => {
+  const correctAnswer = (`${Math.random() * 254},
+  ${Math.random() * 254},
+  ${Math.random() * 254}`);
+
+  rgbTextDisplay.innerText = `(${Math.round(correctAnswer / 100) * 100})`;
+
+  randomize();
+
+  balls[Math.floor(Math.random() * 6)].style.backgroundColor = (`rgb(${correctAnswer})`);
+};
+
+load();
+
+// randomize();
+
+balls.addEventListener('click', (event) => {
+  const itemClicado = event.target;
+  itemClicado.classList.add('answer');
+});
