527125ea405bf5c95a811e1989647036f74476c1
johnpligeiro <johnpligeiro@gmail.com>
2021-04-28 18:57:18 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const balls = document.querySelectorAll('.ball');
 // const ballsContainer = document.querySelectorAll('#balls');
 const rgbTextDisplay = document.getElementById('rgb-color');
+const answer = document.getElementById('answer');
 
 const randomColors = () => (`rgb(${Math.random() * 254},
 ${Math.random() * 254},
@@ -14,22 +15,29 @@ const randomize = () => {
 };
 
 const load = () => {
-  const correctAnswer = (`${Math.random() * 254},
-  ${Math.random() * 254},
-  ${Math.random() * 254}`);
+  // eslint-disable-next-line max-len
+  const correctAnswer = (`${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)}, ${Math.floor(Math.random() * 254)}`);
 
-  rgbTextDisplay.innerText = `(${Math.round(correctAnswer / 100) * 100})`;
-
+  rgbTextDisplay.innerText = `(${correctAnswer})`;
   randomize();
 
   balls[Math.floor(Math.random() * 6)].style.backgroundColor = (`rgb(${correctAnswer})`);
+  balls.forEach((ball) => {
+    ball.addEventListener('click', (event) => {
+      if (event.target.style.backgroundColor === (`rgb(${correctAnswer})`)) {
+        answer.innerText = 'Acertou!';
+      } else {
+        answer.innerText = 'Errou! Tente novamente!';
+      }
+    });
+  });
 };
 
 load();
 
 // randomize();
 
-balls.addEventListener('click', (event) => {
-  const itemClicado = event.target;
-  itemClicado.classList.add('answer');
-});
+// ballsContainer.addEventListener('click', (event) => {
+//   const itemClicado = event.target;
+//   itemClicado.classList.add('answer');
+// });
